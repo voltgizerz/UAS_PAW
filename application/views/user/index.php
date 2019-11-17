5d42af2693f272a5b0b698a4c839ed138f863f5e
voltgizerz <felix.asui@gmail.com>
2019-11-17 21:00:50 +0700

    edit ui little bit

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -3,7 +3,7 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
-    <div class="row ">
+    <div class="row">
         <div class="col-lg-8">
             <?= $this->session->flashdata('message'); ?>
         </div>
@@ -12,7 +12,7 @@
                 <div class="col-md-4-mt-3">
                     <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
                 </div>
-                <div class="col-md-8">
+                <div class="col">
                     <div class="card-body">
                         <h5 class="card-title"><?= $user['name']; ?></h5>
                         <p class="card-text"><?= $user['email']; ?></p>
